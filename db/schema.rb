a57b9447a68885e43616a831c3ba1a1a25ce516a
lockersoft <dave@lockersoft.com>
2013-12-05 22:58:52 -0800

    Added admin to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131206063116) do
+ActiveRecord::Schema.define(version: 20131206065129) do
 
   create_table "users", force: true do |t|
     t.string   "email",                                          default: "", null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20131206063116) do
     t.integer  "coins"
     t.integer  "battles_won"
     t.integer  "battles_lost"
+    t.boolean  "admin"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
